0d49d2348d19e330e5b766467eae630fde03a82b
Juan Carlos <jc.campbellg@gmail.com>
2022-07-05 14:52:07 -0600

    wrong variable name

--- routes/eventsub.js
+++ routes/eventsub.js
@@ -4,15 +4,13 @@ import express from 'express';
 import discordClient from '../discordClient.js';
 const router = express.Router();
 
-router.post('/callback', (req, res, next) => {
-  const notification = req.body.notification;
-  
+router.post('/callback', (req, res, next) => { 
   switch (req.headers['twitch-eventsub-message-type']) {
     case 'webhook_callback_verification':
-      res.status(200).send(notification.challenge);
+      res.status(200).send(req.body.challenge);
       break;
     case 'notification':
-      const event = notification.event;
+      const event = req.body.event;
       res.json(event);
       discordClient.channels.fetch(process.env.DISCORD_NOTIFICATION_CHANNEL).then(channel => {
         channel.send('```'+JSON.stringify(event, undefined, 2)+'```');

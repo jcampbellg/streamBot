b1ccae43769485a1b80ac53c5b7370fb0af1c291
Juan Carlos <jc.campbellg@gmail.com>
2022-07-05 14:36:15 -0600

    less console

--- routes/eventsub.js
+++ routes/eventsub.js
@@ -4,10 +4,7 @@ import express from 'express';
 const router = express.Router();
 
 router.post('/callback', (req, res, next) => {
-  console.log({
-    headers: req.headers,
-    body: req.body
-  });
+  console.log(req.headers['twitch-eventsub-message-type'], req.headers['twitch-eventsub-subscription-type']);
 });
 
 export default router;

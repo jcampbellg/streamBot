c03002e7567fed5dfffa8a2de68a9daea050d17e
Juan Carlos <jc.campbellg@gmail.com>
2022-07-05 14:47:38 -0600

    Add notification

--- routes/eventsub.js
+++ routes/eventsub.js
@@ -1,10 +1,25 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
+import discordClient from '../discordClient.js';
 const router = express.Router();
 
 router.post('/callback', (req, res, next) => {
-  console.log(req.headers['twitch-eventsub-message-type'], req.headers['twitch-eventsub-subscription-type']);
+  const notification = req.body.notification;
+  const event = notification.event;
+  switch (req.headers['twitch-eventsub-message-type']) {
+    case 'webhook_callback_verification':
+      res.status(200).send(notification.challenge);
+      break;
+    case 'notification':
+      res.json(event);
+      discordClient.channels.fetch(process.env.DISCORD_NOTIFICATION_CHANNEL).then(channel => {
+        channel.send('```'+JSON.stringify(event, undefined, 2)+'```');
+      });
+      break;
+    default:
+      res.sendStatus(204);
+  }
 });
 
 export default router;

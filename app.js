bbd74131b3e79ab5b01896d89d6a41a5139a8927
Juan Carlos <jc.campbellg@gmail.com>
2022-07-05 12:28:50 -0600

    add https

--- app.js
+++ app.js
@@ -2,11 +2,23 @@ import cors from 'cors';
 import express from 'express';
 import bodyParser from 'body-parser';
 import http from 'http';
+import https from 'https';
 import dotenv from 'dotenv';
 import discordClient from './discordClient.js';
 dotenv.config();
 
 const PORT = process.env.PORT || 80;
+
+// Certificate
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/jcampbellg.me/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/jcampbellg.me/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/jcampbellg.me/chain.pem', 'utf8');
+
+const credentials = {
+	key: privateKey,
+	cert: certificate,
+	ca: ca
+};
 
 const app = express();
 
@@ -39,10 +51,17 @@ app.get('/.well-known/acme-challenge/wLh1R-NgGZXRVjM6erfijyq08pIStq9dhZE0CafsMD8
 
 // SERVER
 app.set('port', PORT);
-const server = http.createServer(app);
-server.on('listening', () => {
-  console.log('Listening on ' + PORT);
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+
+httpServer.on('listening', () => {
+  console.log('HTTP Listening on ' + PORT);
 });
 
-server.listen(PORT);
+httpsServer.on('listening', () => {
+  console.log('HTTPS Listening on ' + 443);
+});
+
+httpServer.listen(PORT);
+httpsServer.listen(443);
 discordClient.login(process.env.DISCORD_BOT_TOKEN);

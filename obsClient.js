7dff965ceec6a0ebbcd2c9921c260e748c5bc1e9
Juan Carlos <jc.campbellg@gmail.com>
2022-07-07 17:51:11 -0600

    extra line

--- obsClient.js
+++ obsClient.js
@@ -5,7 +5,6 @@ obsClient.on('ConnectionOpened', () => {
   obsClient.send('SetVolume', { source: 'MIC', volume: 0}).catch(err => console.log(err));
   obsClient.send('SetVolume', { source: 'Desktop', volume: 0}).catch(err => console.log(err));
   obsClient.send('SetCurrentScene', {'scene-name': 'Starting' }).catch(err => { console.log(err); });
-  obsClient.send('SetSceneItemRender', {'scene-name': 'Stream Points', source: 'Random Gif', render: false}).catch(err => { console.log(err); });
   obsClient.send('SetSceneItemRender', {'scene-name': 'Live', source: 'Game Source', render: false}).catch(err => { console.log(err); });
   obsClient.send('DeleteSceneItem', {scene: 'Stream Points', item: {name: 'Random Gif'}}).catch(err => { console.log(err); });
 });
